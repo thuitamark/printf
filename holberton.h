2f3c7c68f63641844f48d31f5c967d9c448cde78
thuitamark <mrkthuita@gmail.com>
2022-04-19 17:58:02 +0300

    printf first task

---
+++ holberton.h
@@ -0,0 +1,16 @@
+
+
+
+
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>I
+#include <limits.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+#endif
